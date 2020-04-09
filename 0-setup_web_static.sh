35f840f86e90faa904d453f715e041d8c6638db4
darkares23 <desarrollojuseca@gmail.com>
2020-04-09 14:40:22 -0500

    Sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Sets up your web servers for the deployment of web_static.
+sudo apt-get update && sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo "HBNB testing" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "45i \nlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo service nginx restart
