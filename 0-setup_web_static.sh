8feb7edd299a266c0a87f464eedb889ee76feb1a
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 14:02:11 -0500

    added new script to insert html file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 #Sets up your web servers for the deployment of web_static.
-sudo apt -y update
-sudo apt -y install nginx
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
-echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current 
-chown -R ubuntu:ubuntu /data/
-sed -i '42i location /hbnb_static {\nalias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
-service nginx restart
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update && sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo -e "<html>\n <head>\n </head>\n <body>\n Holberton School\n </body>\n/<html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '42i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
