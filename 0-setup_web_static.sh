eadd75f8382c13339dd7fec7a8bb01ecd38d6de9
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 14:06:13 -0500

    added new script to insert html file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-#Sets up your web servers for the deployment of web_static.
-#!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
+#Sets up your web servers for the deployment of web_static
 sudo apt-get -y update && sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
