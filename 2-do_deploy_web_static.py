7ac5055bda126aa724d6ddd896bfd802a2e0d60f
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 17:47:23 -0500

    correction from task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,31 +3,30 @@
 Fabric script (based on the file 1-pack_web_static.py) that
 distributes an archive to your web servers, using the function do_deploy:
 """
-from fabric.api import run, put, env, sudo
-from fabric.contrib.files import exists
+from fabric.api import local, run, env, put, sudo
 from datetime import datetime
+from os.path import exists
 
 env.hosts = ['34.74.164.242', '3.87.108.35']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    """
-    if not exists(archive_path):
+    """This function will deploy the web static to the servers"""
+    if exists(archive_path) is False:
         return False
-    else:
-        try:
-            put(archive_path, "/tmp/")
-            fileName = archive_path.split("/")[1].split(".")[0]
-            path = "/data/web_static/releases/{}".format(fileName)
-            sudo("mkdir -p {}/".format(path))
-            sudo("tar -zxf /tmp/{}.tgz -C {}/".format(fileName, path))
-            sudo("rm /tmp/{}".format(archive_path.split("/")[1]))
-            sudo("rm /data/web_static/current")
-            sudo("ln -fs /data/web_static/releases/{} /data/web_static/current"
-                 .format(fileName))
-            sudo("service nginx restart")
-        except Exception:
-            return False
-    return True
+    try:
+        put(archive_path, "/tmp/")
+        fileName = archive_path.split("/")[1].split(".")[0]
+        path = "/data/web_static/releases/{}".format(fileName)
+        sudo("mkdir -p {}/".format(path))
+        sudo("tar -zxf /tmp/{}.tgz -C {}/".format(fileName, path))
+        sudo("rm /tmp/{}".format(archive_path.split("/")[1]))
+        sudo("mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}".format(fileName, fileName))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -sf /data/web_static/releases/{}\
+        /data/web_static/current".format(fileName))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
