a12038c9925a4e9cda58d858a2cfdb0f0f4c0b8f
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 18:38:58 -0500

    fixed 22

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,33 @@
-#!/usr/bin/python3
-from fabric.api import local, run, env, put, sudo
+#!/urs/bin/python3
+"""
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy:
+"""
+from fabric.api import run, put, env, sudo
+from fabric.contrib.files import exists
 from datetime import datetime
-from os.path import exists
 
 env.hosts = ['34.74.164.242', '3.87.108.35']
 
 
 def do_deploy(archive_path):
-    if exists(archive_path) is False:
+    """
+    Distributes an archive to web servers
+    """
+    if not exists(archive_path):
         return False
-    try:
-        put(archive_path, "/tmp/")
-        filename = archive_path.split("/")[1].split(".")[0]
-        path = "/data/web_static/releases/{}".format(filename)
-        run("sudo mkdir -p {}/".format(path))
-        run("sudo tar -zxf /tmp/{}.tgz -C {}/".format(filename, path))
-        run("sudo rm /tmp/{}".format(archive_path.split("/")[1]))
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}".format(filename, filename))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/{}\
-        /data/web_static/current".format(filename))
-        print("New version deployed!")
-        return True
-    except Exception:
-        return False
+    else:
+        try:
+            put(archive_path, "/tmp/")
+            fileName = archive_path.split("/")[1].split(".")[0]
+            path = "/data/web_static/releases/{}".format(fileName)
+            sudo("mkdir -p {}/".format(path))
+            sudo("tar -zxf /tmp/{}.tgz -C {}/".format(fileName, path))
+            sudo("rm /tmp/{}".format(archive_path.split("/")[1]))
+            sudo("rm /data/web_static/current")
+            sudo("ln -fs /data/web_static/releases/{} /data/web_static/current"
+                 .format(fileName))
+            sudo("service nginx restart")
+        except Exception:
+            return False
+    return True
