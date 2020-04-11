94ffa4f67b98fbadbb56ea9b0fd3516987fd7a99
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 13:37:40 -0500

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 #Sets up your web servers for the deployment of web_static.
-sudo apt-get update && sudo apt-get install -y nginx
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+sudo apt -y update
+sudo apt -y install nginx
+mkdir /data/
+mkdir /data/web_static/
+mkdir /data/web_static/releases/
+mkdir /data/web_static/shared/
+mkdir /data/web_static/releases/test/
 echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "45i location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-sudo service nginx restart
+ln -sf /data/web_static/releases/test/ /data/web_static/current 
+chown -R ubuntu:ubuntu /data/
+sed -i '42i location /hbnb_static {\nalias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+service nginx restart
