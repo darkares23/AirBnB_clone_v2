20fd68ac901fea58a244788628d1916a965a868e
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 13:34:07 -0500

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ sudo apt-get update && sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "HBNB testing" | sudo tee /data/web_static/releases/test/index.html
+echo -e "<html>\n<head>\n</head>\n<body>\nHolberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "45i location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo sed -i "45i location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
