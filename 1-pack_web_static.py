40a0f8a54c91de6d982a93be3be24ee6cd272c7d
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 15:24:09 -0500

    aded correc compresed file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,7 @@ def do_pack():
     """Create web satic package"""
     try:
         current_date = datetime.now().strftime("%Y%m%d%H%M%S")
-        zipper = "versions/web_static_{}".format(current_date)
+        zipper = "versions/web_static_{}.tgz".format(current_date)
         local("sudo mkdir -p versions | sudo tar -cvzf {} web_static".format(
             zipper))
         return zipper
