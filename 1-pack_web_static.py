4e7443cfcb5ee8a95e680b654e92806454053ff1
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 15:11:52 -0500

    added static packege to server via fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, using the function do_pack
+"""
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Create web satic package"""
+    try:
+        current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+        zipper = "versions/web_static_{}".format(current_date)
+        local("sudo mkdir -p versions | sudo tar -cvzf {} web_static".format(
+            zipper))
+        return zipper
+    except Exception:
+        return None
