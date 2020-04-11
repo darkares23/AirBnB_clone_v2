024c9d9c3075bb7c5a2121697541f2b330d24b64
darkares23 <desarrollojuseca@gmail.com>
2020-04-11 17:48:34 -0500

    fixed task 22

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,16 +16,16 @@ def do_deploy(archive_path):
         return False
     try:
         put(archive_path, "/tmp/")
-        fileName = archive_path.split("/")[1].split(".")[0]
-        path = "/data/web_static/releases/{}".format(fileName)
-        sudo("mkdir -p {}/".format(path))
-        sudo("tar -zxf /tmp/{}.tgz -C {}/".format(fileName, path))
-        sudo("rm /tmp/{}".format(archive_path.split("/")[1]))
-        sudo("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}".format(fileName, fileName))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -sf /data/web_static/releases/{}\
-        /data/web_static/current".format(fileName))
+        filename = archive_path.split("/")[1].split(".")[0]
+        path = "/data/web_static/releases/{}".format(filename)
+        run("sudo mkdir -p {}/".format(path))
+        run("sudo tar -zxf /tmp/{}.tgz -C {}/".format(filename, path))
+        run("sudo rm /tmp/{}".format(archive_path.split("/")[1]))
+        run("sudo mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}".format(filename, filename))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/{}\
+        /data/web_static/current".format(filename))
         print("New version deployed!")
         return True
     except Exception:
