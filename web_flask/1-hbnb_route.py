138b0ee4d1f6acd5b370c5434010018bfd042abf
darkares23 <desarrollojuseca@gmail.com>
2020-04-20 16:44:33 -0500

    added task 0 to 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,7 +10,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def HelloHBNB():
+    """Routes display hello HBNB"""
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Sub route display HBNB"""
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
