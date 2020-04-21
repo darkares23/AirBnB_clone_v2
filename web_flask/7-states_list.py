308e01ede0b2421af7727a49030f95dde407b089
darkares23 <desarrollojuseca@gmail.com>
2020-04-21 14:50:40 -0500

    added new values to staorage

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,27 +4,23 @@ Init the flask aplication
 """
 
 
-from flask import Flask, escape, render_template
+from flask import Flask, render_template
 from models import storage
+
 app = Flask(__name__)
-
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
-app.url_map.strict_slashes = False
-
-
-@app.route('/states_list', strict_slashes=False)
-def show_states_list():
-    """Route to displays all states in db"""
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tearDown(error):
+def tearDown(self):
     """handle teardpown and close the db at request end"""
     storage.close()
 
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Route to displays all states in db"""
+    states_list = storage.all("State").values()
+    return render_template('7-states_list.html', states_list=states_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
