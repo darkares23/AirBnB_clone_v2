d33af6b78c321bdb3f06c9646a1df504a8f57aae
darkares23 <desarrollojuseca@gmail.com>
2020-04-22 13:25:41 -0500

    added task 10 with styles and routes to filters in hbnb template

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -19,13 +19,13 @@ def tearDown(self):
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
 def states():
     """Route display a HTML page: (inside the tag BODY)"""
     states = storage.all('State')
+    cities = storage.all('City')
     amenities = storage.all('Amenity')
     return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities)
+                           states=states, cities=cities, amenities=amenities)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
